95c0ebcb0ab6d2689899b2ef8074574e8a546cdb
Harshal Vaidya <harshalvaidya@Mac.lan>
2025-06-03 21:02:37 -0700

    addFriends method changed

--- src/main/java/edu/usc/bg/workloads/FriendshipWorkload.java
+++ src/main/java/edu/usc/bg/workloads/FriendshipWorkload.java
@@ -17,7 +17,6 @@
  * LICENSE file.                                                                                                                                                                   
  */
 
-
 package edu.usc.bg.workloads;
 
 import java.util.HashMap;
@@ -32,6 +31,7 @@ import edu.usc.bg.base.WorkloadException;
 
 /**
  * Loads friendship for the users
+ * 
  * @author barahman
  *
  */
@@ -42,9 +42,9 @@ public class FriendshipWorkload extends Workload {
 	// The number of fields in a record.
 	public final static int fieldCount = 2;
 	// The name of each field for the "friendship" table.
-	public final static String[] fieldName = {"inviterid", "inviteeid", "status"};
+	public final static String[] fieldName = { "inviterid", "inviteeid", "status" };
 
-	// These following fields could be kept in the property file.	
+	// These following fields could be kept in the property file.
 	// The percentage of pending friend.
 	public static float friendPercentage = 0.01f;
 	// The average number of friends per user.
@@ -65,37 +65,49 @@ public class FriendshipWorkload extends Workload {
 
 	// Initialize all of the threads with the same configuration.
 	public void init(Properties p, Vector<Integer> members) throws WorkloadException {
-		userCount=Integer.parseInt(p.getProperty(Client.USER_COUNT_PROPERTY));
-		avgFriendCount=Integer.parseInt(p.getProperty(Client.FRIENDSHIP_COUNT_PROPERTY));
-		friendPercentage=Float.parseFloat(p.getProperty(Client.CONFPERC_COUNT_PROPERTY));
+		userCount = Integer.parseInt(p.getProperty(Client.USER_COUNT_PROPERTY));
+		avgFriendCount = Integer.parseInt(p.getProperty(Client.FRIENDSHIP_COUNT_PROPERTY));
+		friendPercentage = Float.parseFloat(p.getProperty(Client.CONFPERC_COUNT_PROPERTY));
 		recordCount = userCount * avgFriendCount; // Friendship number.
-		feedLoad = Boolean.parseBoolean(p.getProperty(Client.FEED_LOAD_PROPERTY,Client.FEED_LOAD_DEFAULT_PROPERTY));
+		feedLoad = Boolean.parseBoolean(p.getProperty(Client.FEED_LOAD_PROPERTY, Client.FEED_LOAD_DEFAULT_PROPERTY));
 		flags = new int[userCount];
 		_members = members;
-		for(int i=0; i<userCount; i++)
+		for (int i = 0; i < userCount; i++)
 			flags[i] = 0;
 
 	}
 
 	// Prepare the record for "friendship" table.
 	// inviterid, inviteeid, status.
-	private void addFriends(DB db, int dbKey, int keyNum) {
-		int res = 0;
-		// Generate the fields using StringByteIterator and RandomByteIterator.
-		if ((random.nextInt(userCount) + 1) > userCount * friendPercentage)
-		{		
-			res = db.inviteFriend(dbKey, keyNum); //data = new StringByteIterator("1"); // Pending.
-		}else {					
-			res = db.CreateFriendship(dbKey, keyNum); //data = new StringByteIterator("2"); // Already.
-		}
+	// private void addFriends(DB db, int dbKey, int keyNum) {
+	// int res = 0;
+	// // Generate the fields using StringByteIterator and RandomByteIterator.
+	// if ((random.nextInt(userCount) + 1) > userCount * friendPercentage)
+	// {
+	// res = db.inviteFriend(dbKey, keyNum); //data = new StringByteIterator("1");
+	// // Pending.
+	// }else {
+	// res = db.CreateFriendship(dbKey, keyNum); //data = new
+	// StringByteIterator("2"); // Already.
+	// }
 
-		if(res < 0){
-			System.out.println("The creation of the friendship relationship failes. " +
-					"Please make sure the appropriate schema has been created " + 
-					" and the users have been inserted.");
+	// if(res < 0){
+	// System.out.println("The creation of the friendship relationship failes. " +
+	// "Please make sure the appropriate schema has been created " +
+	// " and the users have been inserted.");
+	// System.exit(-1);
+	// }
+
+	// }
+
+	private void addFriends(DB db, int dbKey, int keyNum) {
+		int res = db.CreateFriendship(dbKey, keyNum);
+		System.out.println(">>> DEBUG: Called CreateFriendship for " + dbKey + " -> " + keyNum);
+
+		if (res < 0) {
+			System.out.println("The creation of the friendship relationship failed.");
 			System.exit(-1);
 		}
-
 	}
 
 	// Prepare the primary key.
@@ -110,56 +122,60 @@ public class FriendshipWorkload extends Workload {
 		int dbIdx = -1;
 		int neededFrnds = avgFriendCount;
 		int upperBoundForIteration;
-		if(feedLoad) {
+		if (feedLoad) {
 			upperBoundForIteration = avgFriendCount;
-		}
-		else {
-			upperBoundForIteration = avgFriendCount/2;
+		} else {
+			upperBoundForIteration = avgFriendCount / 2;
 		}
 		int i;
-		for(i=0; i<userCount; i++){
-			if(flags[i] < upperBoundForIteration){
+		for (i = 0; i < userCount; i++) {
+			if (flags[i] < upperBoundForIteration) {
 				dbKey = _members.get(i);
 				dbIdx = i;
-				/*if(feedLoad) {
-					neededFrnds = avgFriendCount - flags[i];
-				}*/
+				/*
+				 * if(feedLoad) {
+				 * neededFrnds = avgFriendCount - flags[i];
+				 * }
+				 */
 				break;
-			}	
+			}
 		}
-		if(i<userCount && feedLoad) {
+		if (i < userCount && feedLoad) {
 			neededFrnds = avgFriendCount - flags[i];
 		}
-		
-		if(dbKey == -1 || dbIdx == -1){
+
+		if (dbKey == -1 || dbIdx == -1) {
 			return true;
 		}
 
 		int friendid = -1;
 		int friendIdx = -1;
-		
-		if(feedLoad){//for pending friends and conf=0 may not get a uniform number of pending requests per user
-			//popular users in every cluster are friends with popular users (except for the beginning and end of the clusters)
-			//the avergae count is the max number of friend sfor a user and not all users may have the 
-			//same number of friends
-			for(i=0; i<neededFrnds; i++){
-				friendIdx = (dbIdx+i+1);
-				if(friendIdx >userCount-1){
+
+		if (feedLoad) {// for pending friends and conf=0 may not get a uniform number of pending
+						// requests per user
+			// popular users in every cluster are friends with popular users (except for the
+			// beginning and end of the clusters)
+			// the avergae count is the max number of friend sfor a user and not all users
+			// may have the
+			// same number of friends
+			for (i = 0; i < neededFrnds; i++) {
+				friendIdx = (dbIdx + i + 1);
+				if (friendIdx > userCount - 1) {
 					flags[dbIdx] = avgFriendCount;
 					break;
 				}
 				friendid = _members.get(friendIdx);
-				addFriends(db, dbKey,friendid);
-				flags[dbIdx] = flags[dbIdx]+1;
-				flags[friendIdx] = flags[friendIdx]+1;
+				addFriends(db, dbKey, friendid);
+				flags[dbIdx] = flags[dbIdx] + 1;
+				flags[friendIdx] = flags[friendIdx] + 1;
 			}
-		}else{
-			for(i=0; i<(avgFriendCount/2); i++){
-				friendIdx = (dbIdx+i+1) % userCount;
+		} else {
+			for (i = 0; i < (avgFriendCount / 2); i++) {
+				friendIdx = (dbIdx + i + 1) % userCount;
 
 				friendid = _members.get(friendIdx);
-				addFriends(db, dbKey,friendid);
-				flags[dbIdx] = flags[dbIdx]+1;
+				addFriends(db, dbKey, friendid);
+				flags[dbIdx] = flags[dbIdx] + 1;
 			}
 		}
 		return true;
